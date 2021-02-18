89e674a23a117fac4ce0b08eb9703020dd41b590
sangwoo0727 <kangsw9395@gmail.com>
2021-02-19 00:12:08 +0900

    ⚽ Add whistleon project structure using SpringBoot Initializer, https://whistleon.atlassian.net/browse/HALA-38

---
+++ whistleon-back/src/test/java/io/hala/whistleon/WhistleonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hala.whistleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhistleonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
