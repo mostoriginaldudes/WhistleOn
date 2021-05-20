601761d3459006376c17727afbebcc25b07c87e6
sangwoo0727 <kangsw9395@gmail.com>
2021-05-21 00:24:37 +0900

    #24 Remove UnauthorizedException class file
    UnauthorizedException 클래스를 상속받은 클래스 삭제 및 관련된 파일 수정

--- whistleon-back/src/main/java/io/hala/whistleon/common/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package io.hala.whistleon.common.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException() {
-        super("계정이 유효하지 않습니다.");
-    }
-}
--- whistleon-back/src/main/java/io/hala/whistleon/service/login/JwtService.java
+++ whistleon-back/src/main/java/io/hala/whistleon/service/login/JwtService.java
@@ -1,7 +1,5 @@
 package io.hala.whistleon.service.login;
 
-import io.hala.whistleon.common.exception.UnauthorizedException;
-
 public interface JwtService {
     String createToken(String user);
     boolean isUsableToken(String jwt) throws UnauthorizedException;
