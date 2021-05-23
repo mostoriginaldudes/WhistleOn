51cfa23f2e1cafc380b070ed7c30ac8bd55c748d
sangwoo0727 <kangsw9395@gmail.com>
2021-05-23 21:30:08 +0900

    #29 Add exception code about Signup
    회원가입에 필요한 익셉션 핸들링 코드 추가

---
+++ whistleon-back/src/main/java/io/hala/whistleon/common/exception/CustomException.java
@@ -0,0 +1,10 @@
+package io.hala.whistleon.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException{
+    private final ExceptionCode exceptionCode;
+}
