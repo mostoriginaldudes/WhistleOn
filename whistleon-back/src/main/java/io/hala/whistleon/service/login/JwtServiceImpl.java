a503e5a19fcc0374c372fa828327c4e077945c26
sangwoo0727 <kangsw9395@gmail.com>
2021-05-26 01:43:44 +0900

    #29 Update JwtService class

--- whistleon-back/src/main/java/io/hala/whistleon/service/login/JwtServiceImpl.java
+++ whistleon-back/src/main/java/io/hala/whistleon/service/login/JwtServiceImpl.java
@@ -1,12 +1,18 @@
 package io.hala.whistleon.service.login;
 
+import io.hala.whistleon.common.exception.CustomException;
+import io.hala.whistleon.common.exception.ExceptionCode;
+import io.hala.whistleon.domain.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+
+import java.util.Date;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -14,48 +20,41 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Service
 public class JwtServiceImpl implements JwtService {
 
-    @Value("${jwt.header.auth}")
-    private String HEADER_AUTH;
-
     @Value("${jwt.salt.value}")
     private String SALT;
 
     /**
-     *
      * @param user
      * @return jwt token
      */
     @Override
-    public String createToken(String user) {
-//        String jwt = Jwts.builder()
-//                .setHeaderParam("typ", "JWT")
-//                .setHeaderParam("regDate", System.currentTimeMillis())
-//                .setSubject(user.getEmail())
-//                .claim("userId", user.getUserId())
-//                .claim("email", user.getEmail())
-//                .claim("userName", user.getName())
-//                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 60)))
-//                .signWith(SignatureAlgorithm.HS256, this.generateKey())
-//                .compact();
-//        return jwt;
-        return null;
+    public String createToken(User user) {
+        String jwt = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setHeaderParam("regDate", System.currentTimeMillis())
+                .setSubject(user.getEmail())
+                .claim("userId", user.getUserId())
+                .claim("email", user.getEmail())
+                .claim("userName", user.getName())
+                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 60)))
+                .signWith(SignatureAlgorithm.HS256, this.generateKey())
+                .compact();
+        return jwt;
     }
 
     /**
-     *
      * @param jwt
      * @return checking token, then returns true or false.
-     * @throws UnauthorizedException
      */
     @Override
-    public boolean isUsableToken(String jwt) throws UnauthorizedException {
+    public boolean isUsableToken(String jwt) {
         try {
             Jws<Claims> claims = Jwts.parser()
                     .setSigningKey(this.generateKey())
                     .parseClaimsJws(jwt);
             return true;
         } catch (Exception e) {
-            throw new UnauthorizedException();
+            throw new CustomException(ExceptionCode.UNAUTHORIZED_MEMBER);
         }
     }
 
