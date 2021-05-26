b5c934faf7cea3936951146c8b34bfeaaa38aab1
eotkd4791 <eotkd4791@gmail.com>
2021-05-27 02:00:30 +0900

    #42 Update hash router to browser router

--- whistleon-front/src/components/App.jsx
+++ whistleon-front/src/components/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import HomeView from './HomeView.jsx';
 import LoginView from './LoginView.jsx';
 import SignupView from './SignupView.jsx';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Route path="/" component={HomeView} exact />
       <Route path="/login" component={LoginView} />
       <Route path="/signup" component={SignupView} />
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
