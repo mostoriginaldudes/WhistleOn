b2ca85c7c800e787c210421b513ea10243593474
eotkd4791 <eotkd4791@gmail.com>
2021-05-27 01:01:32 +0900

    #42 Add ref to form

--- whistleon-front/src/components/HomeView.jsx
+++ whistleon-front/src/components/HomeView.jsx
@@ -2,45 +2,37 @@ import React from 'react';
 import LinkButton from './common/LinkButton.jsx';
 import '@/style/home.scss';
 
-const HomeView = ({ history }) => {
-  const btnProps = [
-    {
-      dir: 'right',
-      to: '/login',
-      text: '로그인'
-    },
-    {
-      dir: 'right',
-      to: '/signup',
-      text: '회원가입'
-    }
-  ];
+const HomeView = () => {
+  const onClick = () => {
+    console.log('onClick');
+  };
 
   return (
     <div className="home__container">
       <aside className="home__aside">
-        <img
-          src={require('@/assets/images/whistleon-home.png')}
-          alt="whistleon"
-          className="home__img"
-        />
-        <img
-          src={require('@/assets/images/whistleon-logo.png')}
-          alt="whistleon"
-          className="home__img--mobile"
-        />
+        <img src={require('@/assets/images/whistleon-home.png')} alt="whistleon" className="home__img" />
+        <img src={require('@/assets/images/whistleon-logo.png')} alt="whistleon" className="home__img--mobile" />
       </aside>
       <section className="home__section">
         <div className="home__greeting">
           <h2 className="home__title">WHISTLE ON</h2>
           <h3 className="home__subtitle">Good Play, Good People</h3>
-          <article className="home__description">
-            아름다운 축구 문화를 만들기 위해 앞장서는 휘슬온 입니다.
-          </article>
+          <article className="home__description">아름다운 축구 문화를 만들기 위해 앞장서는 휘슬온 입니다.</article>
         </div>
         <div className="home__section__btn-container">
-          {btnProps.map(({ dir, to, text }, index) => (
-            <LinkButton dir={dir} to={to} text={text} key={index} />
+          {[
+            {
+              btnDir: 'right',
+              btnLinkTo: '/login',
+              btnText: '로그인'
+            },
+            {
+              btnDir: 'right',
+              btnLinkTo: '/signup',
+              btnText: '회원가입'
+            }
+          ].map((buttonAttr, index) => (
+            <LinkButton buttonAttr={buttonAttr} key={index} onClick={onClick} />
           ))}
         </div>
       </section>
--- whistleon-front/src/components/LoginView.jsx
+++ whistleon-front/src/components/LoginView.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import InputUnderline from './common/InputUnderline';
 import EventButton from './common/EventButton';
 import '@/style/login-view.scss';
@@ -6,8 +7,8 @@ import '@/style/login-view.scss';
 const LoginView = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const onChange = () => {};
+  const history = useHistory();
+  const loginForm = useRef(null);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -17,17 +18,49 @@ const LoginView = () => {
   return (
     <div className="login__wrapper">
       <h1 className="login__text--title">로그인</h1>
-      <form className="login__form" onSubmit={onSubmit}>
+      <form className="login__form" onSubmit={onSubmit} noValidate ref={loginForm}>
         <ul className="login__info">
           <li className="login__info__input">
-            <InputUnderline type="email" name="이메일" required />
+            <InputUnderline
+              inputObj={{
+                type: 'email',
+                name: '이메일',
+                required: true,
+                value: email,
+                callbacks: {
+                  onInput: (e) => {
+                    setEmail(e.target.value);
+                  }
+                }
+              }}
+            />
           </li>
           <li className="login__info__input">
-            <InputUnderline type="password" name="비밀번호" required />
+            <InputUnderline
+              inputObj={{
+                type: 'password',
+                name: '비밀번호',
+                required: true,
+                value: password,
+                callbacks: {
+                  onInput: (e) => {
+                    setPassword(e.target.value);
+                  }
+                }
+              }}
+            />
           </li>
           <li className="login__info__input">
-            {/* <button className="login__info__btn">로그인</button> */}
-            <EventButton text="로그인" color="yellow" />
+            <EventButton
+              text="로그인"
+              color="yellow"
+              eventHander={() => {
+                loginForm.current.submit();
+              }}
+            />
+          </li>
+          <li className="login__info__input">
+            <EventButton text="회원가입" color="gray" eventHander={history.push('/signup')} />
           </li>
         </ul>
       </form>
