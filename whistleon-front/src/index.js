b3f33c7840e85fee15a3144d486370a3004b3f55
eotkd4791 <eotkd4791@gmail.com>
2021-05-27 00:57:55 +0900

    #42 Update router root

--- whistleon-front/src/index.js
+++ whistleon-front/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+
 import App from './components/App.jsx';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.querySelector('#app')
-);
+ReactDOM.render(<App />, document.querySelector('#app'));
