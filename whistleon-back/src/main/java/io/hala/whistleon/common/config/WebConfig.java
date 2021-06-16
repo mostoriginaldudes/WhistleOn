c33b18cf22acadf19aedcb4791043e32532d168b
sangwoo0727 <kangsw9395@gmail.com>
2021-06-17 03:21:47 +0900

    #54 Remove jwt interceptor code due to using security filter

--- whistleon-back/src/main/java/io/hala/whistleon/common/config/WebConfig.java
+++ whistleon-back/src/main/java/io/hala/whistleon/common/config/WebConfig.java
@@ -1,39 +1,14 @@
 package io.hala.whistleon.common.config;
 
-import io.hala.whistleon.common.interceptor.JwtInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  private static final String[] EXCLUDE_PATH = {};
-
-  private final JwtInterceptor jwtInterceptor;
-
-
-  /**
-   * For using Interceptors, you should add Interceptors on this method.
-   *
-   * @param registry
-   */
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(jwtInterceptor)
-        .addPathPatterns("/test/**")
-        .excludePathPatterns(EXCLUDE_PATH);
-  }
-
-
-  /**
-   * this method is for preventing CORS problem.
-   *
-   * @param registry
-   */
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
