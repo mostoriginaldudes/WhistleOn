34c152e391718d885ef4733b1336f3dc02f607a2
sangwoo0727 <kangsw9395@gmail.com>
2021-06-17 22:41:20 +0900

    #54 Remove unnecessary import code

--- whistleon-back/src/test/java/io/hala/whistleon/service/AuthServiceTest.java
+++ whistleon-back/src/test/java/io/hala/whistleon/service/AuthServiceTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
