ae69965d2730b4ac53bba680b70643f30c5f0293
eotkd4791 <eotkd4791@gmail.com>
2021-06-26 16:10:38 +0900

    #75 Add validation functions

--- whistleon-front/src/components/Signup/FormValidation.js
+++ whistleon-front/src/components/Signup/FormValidation.js
@@ -20,6 +20,31 @@ class FormValidation {
   validatePhoneNum(phoneNum) {
     return /010-\d{4}-\d{4}/g.test(phoneNum);
   }
+
+  validateDate(strDate) {
+    return isFinite(new Date(strDate));
+  }
+
+  validateBirthday(birthday) {
+    return this.validateRange('1950-01-01', birthday, today()) && this.validateDate(birthday);
+  }
+
+  validateLocation(location) {
+    return !!location && location.length > 0;
+  }
+
+  validateWeight(weight) {
+    return this.validateRange(30, weight, 200);
+  }
+
+  validateHeight(height) {
+    return this.validateRange(100, height, 250);
+  }
+
+  validatePosition(main, sub, allPositions) {
+    return main !== sub && allPositions.includes(main) && allPositions.includes(sub);
+  }
+
   validateDescription(description) {
     const descriptionLengthWithoutSpace = description.trim().length;
     return 10 <= descriptionLengthWithoutSpace && descriptionLengthWithoutSpace <= 100;
