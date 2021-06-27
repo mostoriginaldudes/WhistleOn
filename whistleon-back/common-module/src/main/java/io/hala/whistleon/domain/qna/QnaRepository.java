9407a788ab80b8b39dae665df284f2b4d3a325c2
sangwoo0727 <kangsw9395@gmail.com>
2021-06-28 03:51:39 +0900

    #65 Add findAllByUser method in qna repository

--- whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/qna/QnaRepository.java
+++ whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/qna/QnaRepository.java
@@ -1,5 +1,6 @@
 package io.hala.whistleon.domain.qna;
 
+import io.hala.whistleon.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface QnaRepository extends JpaRepository<Qna, Long> {
 
   Page<Qna> findAll(Pageable pageable);
+
+  Page<Qna> findAllByUser(User user, Pageable pageable);
 }
