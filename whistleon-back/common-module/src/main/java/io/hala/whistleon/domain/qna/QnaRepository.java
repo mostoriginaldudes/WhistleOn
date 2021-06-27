9de268fcbc557814ef29fdd539942a45a3c96be6
sangwoo0727 <kangsw9395@gmail.com>
2021-06-28 02:53:37 +0900

    #65 Add findAll method with paging and sorting in qna repository

--- whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/qna/QnaRepository.java
+++ whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/qna/QnaRepository.java
@@ -1,7 +1,10 @@
 package io.hala.whistleon.domain.qna;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
 
+  Page<Qna> findAll(Pageable pageable);
 }
