e876590f42b24a177f6645812c3a4ec3c755fedf
sangwoo0727 <kangsw9395@gmail.com>
2021-07-09 01:02:56 +0900

    #80 Add exception codes

--- whistleon-back/common-module/src/main/java/io/hala/whistleon/exception/ExceptionCode.java
+++ whistleon-back/common-module/src/main/java/io/hala/whistleon/exception/ExceptionCode.java
@@ -11,10 +11,15 @@ public enum ExceptionCode {
   /**
    * Todo 익셉션코드 구체화해서 더 적어야됨
    */
-  RESOURCES_NOT_EXIST(HttpStatus.NOT_FOUND, "요청하신 정보가 존재하지 않습니다"),
   INVALID_FORM_DATA(HttpStatus.BAD_REQUEST, "요청하신 정보가 유효하지 않습니다."),
   FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드 과정 중에 실패하였습니다."),
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류"),
+
+  /**
+   * 404 Not Found
+   */
+  RESOURCES_NOT_EXIST(HttpStatus.NOT_FOUND, "요청하신 정보가 존재하지 않습니다"),
+  TEAM_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 팀이 존재하지 않습니다."),
 
   /**
    * 401 UNAUTHORIZED
@@ -26,6 +31,8 @@ public enum ExceptionCode {
    * 403 FORBIDDEN
    */
   UNAUTHENTICATED_AUTHOR(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+  UNAUTHORIZED_TEAM_MANAGER(HttpStatus.FORBIDDEN, "팀 리더나 매니저가 아닙니다."),
+  UNAUTHORIZED_TEAM(HttpStatus.FORBIDDEN, "해당 팀 소속이 아닙니다."),
 
   /**
    * 409 CONFLICT
