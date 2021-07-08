6f8028c3b480da4b200301dca27d49557ad635d2
sangwoo0727 <kangsw9395@gmail.com>
2021-07-09 01:02:39 +0900

    #80 Update code using optional

--- whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/team/TeamRepository.java
+++ whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/team/TeamRepository.java
@@ -1,8 +1,9 @@
 package io.hala.whistleon.domain.team;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-  Team findByEmail(String email);
+  Optional<Team> findByEmail(String email);
 }
