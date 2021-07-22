d28d4c252e43707ddd0ad93c98831c679646f1d7
sangwoo0727 <kangsw9395@gmail.com>
2021-07-23 05:32:00 +0900

    #83 Add regist request test code

---
+++ whistleon-back/api-module/src/test/java/io/hala/whistleon/team/unit/RegistRequestTest.java
@@ -0,0 +1,83 @@
+package io.hala.whistleon.team.unit;
+
+import io.hala.whistleon.domain.team.Team;
+import io.hala.whistleon.domain.team.TeamMemberRequestRepository;
+import io.hala.whistleon.domain.team.TeamRepository;
+import io.hala.whistleon.domain.user.User;
+import io.hala.whistleon.domain.user.UserRepository;
+import io.hala.whistleon.exception.CustomException;
+import io.hala.whistleon.service.PrincipalHelper;
+import io.hala.whistleon.service.team.TeamServiceImpl;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class RegistRequestTest {
+
+  private User user;
+  private Team team;
+
+  @Mock
+  private PrincipalHelper principalHelper;
+  @Mock
+  private UserRepository userRepository;
+  @Mock
+  private TeamRepository teamRepository;
+  @Mock
+  private TeamMemberRequestRepository teamMemberRequestRepository;
+
+  @InjectMocks
+  private TeamServiceImpl teamService;
+
+
+  private final String email = "test@test.com";
+  private final String name = "테스트";
+  private final String nickname = "테스트닉넴";
+
+  @BeforeEach
+  void beforeTest() {
+    user = User.builder()
+        .userId(1L)
+        .email(email)
+        .name(name)
+        .nickname(nickname)
+        .build();
+
+    team = Team.builder()
+        .teamId(1L)
+        .name("휘슬온팀")
+        .email("whistleon@gmail.com")
+        .build();
+
+    BDDMockito.given(principalHelper.getLoginUser())
+        .willReturn(user);
+  }
+
+  @DisplayName("이미 팀을 가지고 있다면, 팀 가입 요청 실패")
+  @Test
+  void teamRegistRequestFailTest() {
+    user.createTeam(team);
+
+    Assertions.assertThatThrownBy(() -> teamService.registTeamMember(team.getTeamId()))
+        .isInstanceOf(CustomException.class);
+  }
+
+  @DisplayName("팀을 가지고 있지 않다면, 가입 요청 성공")
+  @Test
+  void teamRegistRequestSuccessTest() {
+    BDDMockito.given(teamRepository.findById(1L))
+        .willReturn(Optional.of(team));
+
+    Assertions.assertThatCode(() -> teamService.registTeamMember(team.getTeamId()))
+        .doesNotThrowAnyException();
+  }
+}
