bbe963ca2ab22abdf910b78adf24d6b073053bda
sangwoo0727 <kangsw9395@gmail.com>
2021-07-24 19:20:29 +0900

    #83 Add jpa method on team member request repository

--- whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/team/TeamMemberRequestRepository.java
+++ whistleon-back/common-module/src/main/java/io/hala/whistleon/domain/team/TeamMemberRequestRepository.java
@@ -1,7 +1,13 @@
 package io.hala.whistleon.domain.team;
 
+import io.hala.whistleon.domain.user.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamMemberRequestRepository extends JpaRepository<TeamMemberRequest, Long> {
 
+  List<TeamMemberRequest> findAllByTeam(Team team);
+
+  Optional<TeamMemberRequest> findByUserAndTeam(User user, Team team);
 }
